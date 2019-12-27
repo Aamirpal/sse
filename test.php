0bffd8801c009268698e0707a926847f8db19848
pankaj <pankaj@ucreate.co.in>
2019-12-27 11:56:35 +0530

    changes

--- test.php
+++ test.php
@@ -4,12 +4,10 @@
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-echo "data:" . date('r');
-flush();
-
-
-
 $time = $_GET['token'];
-
-echo "data: The server time is: {$time}\n\n";
-flush();
+while (true) {
+    ob_clean();
+    echo "data: The server time is: {$time}\n\n";
+    sleep(1000);
+    flush();
+}
