65d057ba44560142328bd05ef7d0ae699bbc2025
pankaj <pankaj@ucreate.co.in>
2019-12-27 13:05:42 +0530

    changes

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ function setInterval($f, $milliseconds)
 {
     $seconds = (int) $milliseconds / 1000;
     $i = 0;
-    while ($i < 1000) {
+    while ($i < 5) {
         $f();
         $i++;
         sleep($seconds);
@@ -12,12 +12,14 @@ function setInterval($f, $milliseconds)
 }
 
 header('Content-Type: text/event-stream');
+header('Connection: keep-alive');
 header('Cache-Control: no-cache');
 
 $time = $_GET['token'];
 
 setInterval(function () {
     ob_clean();
-    echo "data: The server time is: {$time}\n\n";
+    echo "data: The server time is:" . date('r') . "\n\n";
+    ob_flush();
     flush();
 }, 1000);
