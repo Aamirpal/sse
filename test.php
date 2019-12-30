d1a851d84341b465ad3b03a13bdf1a96b8fcf08d
Pankaj Vashisht <pankaj@ucreate.co.in>
2019-12-30 18:32:29 +0530

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ if (function_exists('apache_setenv')) {
     @apache_setenv('no-gzip', 1);
 }
 @ini_set('zlib.output_compression', 0);
-@ini_set('implicit_flush', 5);
+@ini_set('implicit_flush', 1);
 // while (ob_get_level() != 0) {
 //     ob_end_flush();
 // }
